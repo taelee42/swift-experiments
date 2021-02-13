8e77fcc8c3d24339557ae574ceb1413f5e78d5be
taelee <taelee@student.42seoul.kr>
2021-02-13 12:14:57 +0900

    006navigation/single_storyboard: starter

---
+++ 006navigation/00single_storyboard/single_storyboard/single_storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  single_storyboard
+//
+//  Created by Terry Lee on 2021/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
