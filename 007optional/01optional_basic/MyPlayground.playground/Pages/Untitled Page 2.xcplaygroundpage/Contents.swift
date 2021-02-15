041eafaf137b54bdb95e86d47d6b84d34bc2442f
taelee <taelee@student.42seoul.kr>
2021-02-15 23:14:37 +0900

    007optional: - optional_basic, optional_binding 폴더 만들고 초기세팅

---
+++ 007optional/01optional_basic/MyPlayground.playground/Pages/Untitled Page 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var name:String? = "batman"
+
+print(name)
+
+
+let number = Int("I'm not a number")
+print(number)
+
+
+let number1 = Int("10")
+
+let number2 = Int("42")
+
+print(number1! + number2!)
+
