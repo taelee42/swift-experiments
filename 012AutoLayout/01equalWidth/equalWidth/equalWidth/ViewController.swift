2ecaccf1f5cf459cd397af48a03f45e52e2f5101
taelee <taelee@student.42seoul.kr>
2021-02-24 16:55:25 +0900

    012AutoLayout: - AutoLayout equal width 실습

---
+++ 012AutoLayout/01equalWidth/equalWidth/equalWidth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  equalWidth
+//
+//  Created by Terry Lee on 2/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
