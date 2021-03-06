127699370c85c74c27f227e0987c49ca34cb2fec
taelee <taelee@student.42seoul.kr>
2021-03-06 15:06:36 +0900

    016realm: - install realm - specify platform version to iOS13

---
+++ 016realm/realmTutorial/Pods/Target Support Files/Pods-realmTutorial/Pods-realmTutorial-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_realmTutorial : NSObject
+@end
+@implementation PodsDummy_Pods_realmTutorial
+@end
---
+++ 016realm/realmTutorial/realmTutorial/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  realmTutorial
+//
+//  Created by TaeHyeok LEE on 2021/03/06.
+//
+
+import UIKit
+import RealmSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    let app = App(id: "realmTutorial") // Replace YOUR_REALM_APP_ID with your Realm app ID
+
+
+    
+    
+}
+
